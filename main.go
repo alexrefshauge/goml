307614eb07467e76704c950de9e107b1317b4c34
alexrefshauge <arefsh22@student.aau.dk>
2023-07-26 16:27:51 +0200

    tests

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	"time"
 )
 
-const learningRate float64 = 0.5
+const learningRate float64 = 0.1
 
 func rgbToGrey(r float64, g float64, b float64) float64 {
 	return 0.299*r + 0.587*g + 0.114*b
@@ -34,7 +34,9 @@ func generateImage(path string, size int, net Network) {
 			output := net.Activation[net.LayerCount-1]
 			col := output.Data[0]
 			r := uint8(col[0] * 255)
-			img.Set(x, y, color.Gray{r})
+			g := uint8(col[1] * 255)
+			b := uint8(col[2] * 255)
+			img.Set(x, y, color.RGBA{r, g, b, 255})
 		}
 	}
 
@@ -59,47 +61,70 @@ func main() {
 	cycles := 50000
 	network := NetworkNew([]Layer{
 		{NeuronCount: 2, ActivationFunc: TanH},
-		{NeuronCount: 28, ActivationFunc: ReLU},
-		{NeuronCount: 28, ActivationFunc: ReLU},
-		{NeuronCount: 1, ActivationFunc: TanH},
+		{NeuronCount: 8, ActivationFunc: ReLU},
+		{NeuronCount: 3, ActivationFunc: TanH},
 	}, func() float64 { return rand.Float64()*0.4 - 0.2 },
 		func() float64 { return rand.Float64()*2 - 1 },
 	)
 
-	trainingData := MatNew(resWidth*resHeight, 3, Zero)
+	trainingData := MatNew(resWidth*resHeight, 5, Zero)
 	trainingDataIn := MatNew(resWidth*resHeight, 2, Zero)
-	trainingDataOut := MatNew(resWidth*resHeight, 1, Zero)
+	trainingDataOut := MatNew(resWidth*resHeight, 3, Zero)
 	for y := 0; y < resHeight; y++ {
 		for x := 0; x < resWidth; x++ {
 			pixel := trainingImage.At(x, y)
 			r, g, b, _ := pixel.RGBA()
 			xx := float64(x+1) / float64(resWidth)
 			yy := float64(y+1) / float64(resHeight)
-			brightness := rgbToGrey(float64(r>>8), float64(g>>8), float64(b>>8)) / float64(255)
-			trainingData.Data[y*resWidth+x] = []float64{xx, yy, brightness}
+			//brightness := rgbToGrey(float64(r>>8), float64(g>>8), float64(b>>8)) / float64(255)
+			trainingData.Data[y*resWidth+x] = []float64{
+				xx,
+				yy,
+				float64(r>>8) / 255,
+				float64(g>>8) / 255,
+				float64(b>>8) / 255,
+			}
 		}
 	}
 
+	var rate float64 = learningRate
+	var volatility float64 = 0
+	var cost float64
 	for i := 0; i < cycles; i++ {
 		MatRowShuffle(&trainingData)
 		for row := 0; row < trainingData.Rows; row++ {
 			trainingDataIn.Data[row][0] = trainingData.Data[row][0]
 			trainingDataIn.Data[row][1] = trainingData.Data[row][1]
 			trainingDataOut.Data[row][0] = trainingData.Data[row][2]
-		}
-
-		var rate float64 = learningRate
-		if i > 3000 {
-			rate = 0.1
+			trainingDataOut.Data[row][1] = trainingData.Data[row][3]
+			trainingDataOut.Data[row][2] = trainingData.Data[row][4]
 		}
 		network.Backprop(trainingDataIn, trainingDataOut, rate)
-		fmt.Printf("epoch: %07d rate: %0.02f cost: %.03f \n", i, rate, network.Cost(trainingDataIn, trainingDataOut))
+		oldCost := cost
+		cost = network.Cost(trainingDataIn, trainingDataOut)
+		fmt.Printf("epoch: %07d rate: %0.02f cost: %.03f vol: %0.02f\n", i, rate, cost, volatility)
 
 		if i%10 == 0 {
-			generateImage("progress.png", 12, network)
+			generateImage("progress.png", 32, network)
 		}
 		if i%1000 == 0 {
 			generateImage("result.png", 128, network)
+		}
+
+		//adjust rate according to volatility
+		if i > 300 {
+			rate = 0.05
+			deltaCost := cost - oldCost
+			if volatility > 0 {
+				volatility--
+			}
+			if deltaCost > 0 {
+				volatility += 2
+			}
+
+			if volatility > 2 {
+				rate /= 1.001
+			}
 		}
 	}
 	generateImage("progress.png", 1024, network)
